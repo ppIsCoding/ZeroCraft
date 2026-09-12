bbcf9b475737fc74b0abdf3ee52a7b4f13910712
pp12687651 <3289609037@qq.com>
2026-09-12 23:09:06 +0800

    用户管理

--- src/main/java/com/pp/zerocraft/ZeroCraftApplication.java
+++ src/main/java/com/pp/zerocraft/ZeroCraftApplication.java
@@ -1,12 +1,14 @@
 package com.pp.zerocraft;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.pp.zerocraft.mapper")
 public class ZeroCraftApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/pp/zerocraft/config/JsonConfig.java
@@ -0,0 +1,25 @@
+package com.pp.zerocraft.config;
+
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring MVC Json 配置
+ */
+@Configuration
+public class JsonConfig {
+
+    /**
+     * 添加 Long 转 json 精度丢失的配置
+     */
+    @Bean
+    public Module longToStringModule() {
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        return module;
+    }
+}
