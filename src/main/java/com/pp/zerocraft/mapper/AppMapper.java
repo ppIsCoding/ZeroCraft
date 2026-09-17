a9ed7568977ae02a0fc8a274d832f0cbff21700a
pp12687651 <3289609037@qq.com>
2026-09-17 21:23:56 +0800

    应用开发完成

---
+++ src/main/java/com/pp/zerocraft/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package com.pp.zerocraft.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.pp.zerocraft.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author <a href="https://github.com/ppiscoding">pp</a>
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
---
+++ src/main/java/com/pp/zerocraft/service/AppService.java
@@ -0,0 +1,27 @@
+package com.pp.zerocraft.service;
+
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import com.pp.zerocraft.model.dto.app.AppQueryRequest;
+import com.pp.zerocraft.model.entity.App;
+import com.pp.zerocraft.model.entity.User;
+import com.pp.zerocraft.model.vo.AppVO;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+/**
+ * 应用 服务层。
+ *
+ * @author <a href="https://github.com/ppiscoding">pp</a>
+ */
+public interface AppService extends IService<App> {
+
+    AppVO getAppVO(App app);
+
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    List<AppVO> getAppVOList(List<App> appList);
+
+    Flux<String> chatToGenCode(Long appId, String message, User loginUser);
+}
