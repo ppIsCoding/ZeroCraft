a20130b78faff3134037d15d6bb1c9c5790bd5c9
pp12687651 <3289609037@qq.com>
2026-09-21 15:26:59 +0800

    允许用户可以下载应用源码（有文件过滤功能，不会下载到不需要的文件）

---
+++ src/main/java/com/pp/zerocraft/service/ProjectDownloadService.java
@@ -0,0 +1,7 @@
+package com.pp.zerocraft.service;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface ProjectDownloadService {
+    void downloadProjectAsZip(String projectPath, String downloadFileName, HttpServletResponse response);
+}
